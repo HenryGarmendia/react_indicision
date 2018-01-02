8b61ec55f5ea2919fa53f2910de3721c858277d3
HenryGarmendia <me@henrygarmendia.com>
2018-01-02 03:35:03 -0600

    modified the app.js, added npm init and .gitignore file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
-console.log('WE CONNECTED');
+"use strict";
+
+// JSX - JavaScript XML
+// var template = <p>This is a JSX from app.js...</p>;
+
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is a JSX from app.js..."
+);
+var app_roor = document.getElementById('app');
+
+ReactDOM.render(template, app_roor);
