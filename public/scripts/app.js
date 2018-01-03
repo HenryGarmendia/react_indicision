3c1b2c595c9d3356dee8dedacf86ec4d3ef467a1
HenryGarmendia <me@henrygarmendia.com>
2018-01-02 18:55:10 -0600

    added an ol list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,8 +14,28 @@ var template = React.createElement(
         "p",
         null,
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "List item 1"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "List item 2"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "List item 3"
+        )
     )
 );
+
 var app_roor = document.getElementById('react_container');
 
 ReactDOM.render(template, app_roor);
--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@
 var template = (
     <div>
     	<h1>Indecision Web-App</h1>
-    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <ol>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ol>
     </div>
 );
+
 var app_roor = document.getElementById('react_container');
 
 ReactDOM.render(template, app_roor);
