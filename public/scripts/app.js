4b837bfab3d5e32a5a7b6f4f3575bc4fd5c23bde
HenryGarmendia <me@henrygarmendia.com>
2018-01-03 15:39:46 -0600

    change some syntax to ES6 and minor changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,40 +2,83 @@
 
 // JSX - JavaScript XML
 
+var app = {
+    title: 'Indecision Web-App',
+    subtitle: 'Put your life decision in the logic of a computer.',
+    options: ['One', 'Two', 'Three', 'Four']
+};
 var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        "Indecision Web-App"
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
-        "ol",
+        'ol',
         null,
         React.createElement(
-            "li",
+            'li',
             null,
-            "List item 1"
+            'List item 1'
         ),
         React.createElement(
-            "li",
+            'li',
             null,
-            "List item 2"
+            'List item 2'
         ),
         React.createElement(
-            "li",
+            'li',
             null,
-            "List item 3"
+            'List item 3'
         )
     )
 );
 
-var app_roor = document.getElementById('react_container');
+var user = {
+    name: 'Henry',
+    age: 37,
+    location: 'Forney'
+};
 
+var get_location = function get_location(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    get_location(user.location)
+);
+
+var app_roor = document.getElementById('react_container');
 ReactDOM.render(template, app_roor);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 "use strict";
 
 // JSX - JavaScript XML
-var template = (
+const app = {
+    title: 'Indecision Web-App',
+    subtitle: 'Put your life decision in the logic of a computer.',
+    options: ['One', 'Two', 'Three', 'Four']
+};
+const template = (
     <div>
-    	<h1>Indecision Web-App</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    	<h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
         <ol>
             <li>List item 1</li>
             <li>List item 2</li>
@@ -13,6 +19,25 @@ var template = (
     </div>
 );
 
-var app_roor = document.getElementById('react_container');
+const user = {
+    name: 'Henry',
+    age: 37,
+    location: 'Forney'
+};
 
+const get_location = function(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+const templateTwo = (
+    <div>
+        <h1>{ user.name ? user.name : 'Anonymous' }</h1>
+        {(user.age && user.age >= 18) && <p>Age: { user.age }</p>}
+        {get_location(user.location)}
+    </div>
+);
+
+const app_roor = document.getElementById('react_container');
 ReactDOM.render(template, app_roor);
