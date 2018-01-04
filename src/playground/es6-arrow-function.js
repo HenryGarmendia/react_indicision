1a4663a8f8ef3f30070db2092faa19968e881368
HenryGarmendia <me@henrygarmendia.com>
2018-01-03 23:42:09 -0600

    added a new file to test arrow functions

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,23 +1,19 @@
-'use strict';
-
 /* 
     An arrow function expression has a shorter syntax than a function expression and does not have its own this, arguments, super, or new.target. 
-*/
+*/ 
 // if is an arrow function and only returns one expression you can do in one line e.g. const square = (x) =>  x * x;
-var square = function square(x) {
+const square = (x) => {
     return x * x;
-};
+}
 
 console.log(square(9));
 
 // normal arrow function
-var get_first_name = function get_first_name(full_name) {
+const get_first_name = (full_name) => {
     return full_name.split(' ')[0];
 };
 console.log(get_first_name('Henry Garmendia'));
 
 // shorthand arrow function
-var get_first_name2 = function get_first_name2(full_name) {
-    return full_name.split(' ')[0];
-};
+const get_first_name2 = (full_name) => full_name.split(' ')[0];
 console.log(get_first_name2('Henry Garmendia'));
