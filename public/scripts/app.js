a7de68c95fd051759ca317c2640f7f4a03adae94
HenryGarmendia <me@henrygarmendia.com>
2018-01-04 04:09:21 -0600

    added remove_all, on_form_submit, render_options and a form, capture the value from the input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,47 +5,88 @@
 var app = {
     title: 'Indecision Web-App',
     subtitle: 'Put your life decision in the logic of a computer.',
-    options: ['One', 'Two', 'Three']
+    options: []
 };
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'List item 1'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'List item 2'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'List item 3'
-        )
-    )
-);
+
+var on_form_submit = function on_form_submit(e) {
+    e.preventDefault();
+    var option = e.currentTarget.options.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.options.value = '';
+        render_options();
+    }
+};
+
+var on_remove_all = function on_remove_all() {
+    app.options = [];
+    render_options();
+};
 
 var app_root = document.getElementById('react_container');
 
-ReactDOM.render(template, app_root);
+var render_options = function render_options() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        app.options.length > 0 ? React.createElement(
+            'button',
+            { onClick: on_remove_all },
+            'Remove All'
+        ) : null,
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'List item 1'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'List item 2'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'List item 3'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: on_form_submit },
+            React.createElement('input', { type: 'text', name: 'options', placeholder: 'e.g. Better at JavaScript' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, app_root);
+};
+
+render_options();
--- src/app.js
+++ src/app.js
@@ -4,21 +4,48 @@
 const app = {
     title: 'Indecision Web-App',
     subtitle: 'Put your life decision in the logic of a computer.',
-    options: ['One', 'Two', 'Three']
+    options: []
 };
-const template = (
-    <div>
-    	<h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ol>
-    </div>
-);
+
+const on_form_submit = (e) => {
+    e.preventDefault();
+    const option = e.currentTarget.options.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.options.value = '';
+        render_options();
+    }
+};
+
+const on_remove_all = () => {
+    app.options = [];
+    render_options();
+};
 
 const app_root = document.getElementById('react_container');
 
-ReactDOM.render(template, app_root);
+const render_options = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            {(app.options.length > 0) ? <button onClick={on_remove_all}>Remove All</button> : null }
+            <ol>
+                <li>List item 1</li>
+                <li>List item 2</li>
+                <li>List item 3</li>
+            </ol>
+            <form onSubmit={on_form_submit}>
+                <input type="text" name="options" placeholder="e.g. Better at JavaScript" />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, app_root);
+}
+
+render_options();
