59e1aaf2d045e26dcd8cfbc50688068fdc3597ef
HenryGarmendia <me@henrygarmendia.com>
2018-01-07 02:26:00 -0600

    added webpack-dev-server to project

--- src/app.js
+++ src/app.js
@@ -2,5 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionWebApp from './components/indecision_web_app'
 
-
 ReactDOM.render(<IndecisionWebApp />, document.getElementById('react_container'));
