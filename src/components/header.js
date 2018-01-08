131c652d81974047fcb593a900349438868d8138
HenryGarmendia <me@henrygarmendia.com>
2018-01-07 22:17:39 -0600

    did the same implicit return to the header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.app_title}</h1>
-            {props.sub_title && <p>{props.sub_title}</p>}
-        </div>
-    );
-}
+const Header = (props) => (
+    <div>
+        <h1>{props.app_title}</h1>
+        {props.sub_title && <p>{props.sub_title}</p>}
+    </div>
+);
 
 Header.defaultProps = {
     app_title: 'Indecision Web-App'
