4c235395c9ab06d479110c935c581d2b84776c01
HenryGarmendia <me@henrygarmendia.com>
2018-01-07 23:45:48 -0600

    add some basic styles to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
+    <div className="header">
         <h1>{props.app_title}</h1>
         {props.sub_title && <p>{props.sub_title}</p>}
     </div>
