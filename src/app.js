c64f40c7bb461269cccbc72e8926a59bd0c736a2
HenryGarmendia <me@henrygarmendia.com>
2018-01-07 23:24:30 -0600

    install sass-loader and node-sass to the project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionWebApp from './components/indecision_web_app'
-import './styles/styles.css'
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionWebApp />, document.getElementById('react_container'));
