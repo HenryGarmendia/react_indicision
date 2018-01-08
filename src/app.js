b18fd774a4245362717b6f07f74cee6b70b60558
HenryGarmendia <me@henrygarmendia.com>
2018-01-08 00:00:42 -0600

    install normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import IndecisionWebApp from './components/indecision_web_app'
-import './styles/styles.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionWebApp from './components/indecision_web_app';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionWebApp />, document.getElementById('react_container'));
