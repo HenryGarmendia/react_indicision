f1269c701622646b2d91b2110fbb5715dc5eb9a8
HenryGarmendia <me@henrygarmendia.com>
2018-01-09 09:14:10 -0600

    finalize the styles for the project

--- src/components/option_modal.js
+++ src/components/option_modal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
 		onRequestClose={props.method_clear_selected}
 		ariaHideApp={false}
 		contentLabel="Selected Option"
+		closeTimeoutMS={200}
+		className="modal"
 	>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.method_clear_selected}>Okay</button>
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+		<button className="button" onClick={props.method_clear_selected}>Okay</button>
 	</Modal>
 );
 
